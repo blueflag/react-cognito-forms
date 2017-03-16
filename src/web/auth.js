89015b4f7ebb9037f2bd2a0714a8c085e292d768
Allan Hortle <allanhortle@gmail.com>
2017-03-16 12:08:28 +1100

    Add test to local storage to make sure the stored object is json

--- src/web/auth.js
+++ src/web/auth.js
@@ -3,12 +3,22 @@ import Auth from '../auth';
 
 var STORAGE_KEY = 'react-cognito-forms';
 
+function testJSON(tokenString: string) {
+    try {
+        JSON.parse(tokenString);
+        return tokenString;
+    } catch(e) {
+        localStorage.removeItem(STORAGE_KEY);
+        console.warn('Could not parse token string.');
+    }
+}
+
 const auth = new Auth({
     async storeToken(token: string): Promise {
-        return localStorage.setItem(STORAGE_KEY, token);
+        return localStorage.setItem(STORAGE_KEY, testJSON(token));
     },
     async retrieveToken(): Promise<string> {
-        return localStorage.getItem(STORAGE_KEY);
+        return testJSON(localStorage.getItem(STORAGE_KEY));
     }
 });
 
