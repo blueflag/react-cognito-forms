4fbedb241a3166fd6e9f90a99340961a1fce67ae
Damien Clarke <dxinteractive@gmail.com>
2017-12-15 13:42:17 +1100

    Export forgot password forms

---
+++ src/web/components/ForgotPasswordConfirmForm.jsx
@@ -0,0 +1,52 @@
+/* @flow */
+
+import React from 'react';
+import Input from 'stampy/lib/input/input/Input';
+import Button from 'stampy/lib/component/button/Button';
+import Label from 'stampy/lib/component/field/Label';
+import Messages from './Messages';
+
+export default function ForgotPasswordConfirmForm(props: Object): React.Element {
+    const {
+        confirmationCode,
+        confirmIntro,
+        errors,
+        loginPath,
+        onChange,
+        onConfirm,
+        password
+    } = props;
+
+    return <div>
+        {confirmIntro}
+        <form autoComplete="off" className="ReactCognitoForm" onSubmit={onConfirm} method="post">
+            <Label spruceName="ReactCognitoFormLabel">Verification Code</Label>
+            <Input
+                spruceName="ReactCognitoFormInput"
+                modifier="text"
+                type="text"
+                name="confirmationCode"
+                placeholder="e.g. 12345678"
+                value={confirmationCode}
+                onChange={onChange('confirmationCode')}
+                inputProps={{autocomplete: "off"}}
+            />
+            <Label spruceName="ReactCognitoFormLabel">New Password</Label>
+            <Input
+                spruceName="ReactCognitoFormInput"
+                modifier="text"
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={onChange('password')}
+                inputProps={{autocomplete: "off"}}
+            />
+            <Button spruceName="ReactCognitoFormButton" type="submit">Change Password</Button>
+        </form>
+        <Messages errors={errors} />
+        <div>
+            {loginPath && <a className="ReactCognitoFormLink ReactCognitoFormLink-login" href={loginPath}>Back to login</a>}
+        </div>
+    </div>;
+}
